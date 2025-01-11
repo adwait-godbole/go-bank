bbc978b69a35ac67fbb73dd5db9467100dac71c0
adwait-godbole <adwaitngodbole@gmail.com>
2025-01-11 13:35:38 +0530

    add Querier interface

---
+++ api/account_test.go
@@ -0,0 +1,126 @@
+package api
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/adwait-godbole/go-bank/db/mock"
+	db "github.com/adwait-godbole/go-bank/db/sqlc"
+	"github.com/adwait-godbole/go-bank/util"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+	account := randomAccount()
+
+	testCases := []struct {
+		name          string
+		accountID     int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:      "BadRequest",
+			accountID: 0,
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					GetAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, recorder.Code, http.StatusBadRequest)
+			},
+		},
+		{
+			name:      "NotFound",
+			accountID: account.ID,
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)). // I expect the GetAccount function of the store to be called with any context and this specific account ID argument
+					Times(1).
+					Return(db.Account{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, recorder.Code, http.StatusNotFound)
+			},
+		},
+		{
+			name:      "InternalServerError",
+			accountID: account.ID,
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)). // I expect the GetAccount function of the store to be called with any context and this specific account ID argument
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, recorder.Code, http.StatusInternalServerError)
+			},
+		},
+		{
+			name:      "OK",
+			accountID: account.ID,
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)). // I expect the GetAccount function of the store to be called with any context and this specific account ID argument
+					Times(1).
+					Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, recorder.Code, http.StatusOK)
+				requireBodyMatchAccount(t, recorder.Body, account)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStore := mockdb.NewMockStore(ctrl)
+			// build stubs
+			tc.buildStubs(mockStore)
+
+			// start test server and send request
+			server := NewServer(mockStore)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/accounts/%d", tc.accountID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+}
+
+func requireBodyMatchAccount(t testing.TB, body *bytes.Buffer, account db.Account) {
+	t.Helper()
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccount db.Account
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, account, gotAccount)
+}
